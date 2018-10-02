c91128c5a84b787f3419dfc77bd3669334278cbf
Angus Hollands <goosey15@gmail.com>
2018-10-02 11:34:27 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example_pkg",
+    version="0.0.1",
+    author="Angus Hollands",
+    author_email="goosey15@gmail.com",
+    description="A simple python Latex generator.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/agoose77/texy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
