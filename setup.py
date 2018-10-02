c683717d8f32ef8a780fd7d734a3c7eb3a8619c2
Angus Hollands <goosey15@gmail.com>
2018-10-02 11:35:32 +0100

    Fix name in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example_pkg",
+    name="text",
     version="0.0.1",
     author="Angus Hollands",
     author_email="goosey15@gmail.com",
